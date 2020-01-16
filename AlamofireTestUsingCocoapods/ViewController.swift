3aff0e4098d88aa1354cf1144da34a2ac446325a
Md. Mehedi Hasan Akash <macMehi@Labs-Mac.local>
2020-01-16 17:22:42 +0600

    initial commit

---
+++ AlamofireTestUsingCocoapods/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AlamofireTestUsingCocoapods
+//
+//  Created by Md. Mehedi Hasan Akash on 1/16/20.
+//  Copyright © 2020 Md. Mehedi Hasan Akash. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var showTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func getButtonTapped(_ sender: Any) {
+        // Alamofire 4
+        Alamofire.request("https://jsonplaceholder.typicode.com/posts").response { response in // method defaults to `.get`
+            debugPrint(response)
+            
+            self.showTextView.text = String(bytes: response.data!, encoding: .utf8)
+        }
+    }
+    
+}
+
